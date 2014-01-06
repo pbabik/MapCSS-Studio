25cf9ca3bd1e4ab26139834818ea32f9d5a16c9a
pbabik <michalmacki@gmail.com>
2014-01-06 14:49:49 +0100

    switch to Kothic renderer, add sample JS style

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 /* Configuration variables - adjust to your deployment and/or region */
 
 // MAP EXTENT
-var startX = 0; // X coordinate of initial map center, in decimal degrees
-var startY = 0; // Y coordinate of initial map center, in decimal degrees
-var startZ = 1; // initial map zoom level
+var startX = 20; // X coordinate of initial map center, in decimal degrees
+var startY = 50; // Y coordinate of initial map center, in decimal degrees
+var startZ = 15; // initial map zoom level
 
 // TILES URL
 
-var vtileUrl = "http://127.0.0.1/vtile/{z}/{x}/{y}.js";
+var vtileUrl = "http://127.0.0.1/vtile/{z}/{x}/{y}";
 
