310a66dc2d173edfac9afc296ffabfe160dc374a
pbabik <michalmacki@gmail.com>
2014-01-06 14:08:28 +0100

    libs upgrade

--- src/kothic-leaflet.js
+++ src/kothic-leaflet.js
@@ -1,80 +1,139 @@
 L.TileLayer.Kothic = L.TileLayer.Canvas.extend({
-	options: {
-		tileSize: 256 * 4,
-		zoomOffset: 2,
-		minZoom: 2,
-		maxZoom: 22,
-		updateWhenIdle: true,
-		unloadInvisibleTiles: true,
-		attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Rendering by <a href="http://github.com/kothic/kothic-js">Kothic JS</a>',
-		async: true,
-		buffered: true
-	},
+    options: {
+        tileSize: 256 * 4,
+        zoomOffset: 2,
+        minZoom: 2,
+        maxZoom: 22,
+        updateWhenIdle: false,
+        unloadInvisibleTiles: false,
+        attribution: 'Map data &copy; 2013 <a href="http://osm.org/copyright">OpenStreetMap</a> contributors,' +
+                     ' Rendering by <a href="http://github.com/kothic/kothic-js">Kothic JS</a>',
+        async: true,
+        buffered: false,
+        styles: MapCSS.availableStyles
+    },
 
-	initialize: function(options) {
-		L.Util.setOptions(this, options);
+    initialize: function(url,options) {
+        L.Util.setOptions(this, options);
 
-		this._canvases = {};
-		this._debugMessages = [];
+        this._url = url;
+        this._canvases = {};
+        this._scripts = {};
+        this._debugMessages = [];
 
-		var layer = this;
+        window.onKothicDataResponse = L.Util.bind(this._onKothicDataResponse, this);
+    },
 
-		window.onKothicDataResponse = L.Util.bind(this._onKothicDataResponse, this);
-	},
+    _onKothicDataResponse: function(data, zoom, x, y) {
+        var key = [zoom, x, y].join('/'),
+            canvas = this._canvases[key],
+            zoomOffset = this.options.zoomOffset,
+            layer = this;
 
-	_onKothicDataResponse: function(data, zoom, x, y) {
-		var key = [zoom, x, y].join('/'),
-			canvas = this._canvases[key],
-			buffered = this.options.buffered,
-			zoomOffset = this.options.zoomOffset,
-			layer = this;
+        if (!canvas) {
+            return;
+        }
 
-		function onRenderComplete(debugInfo) {
-			// TODO move this logic outside
-			var debugStr = layer.getDebugStr(debugInfo, x, y, zoom);
-			layer._debugMessages.push(debugStr);
-			layer.tileDrawn(canvas);
-		}
+        function onRenderComplete() {
+            layer.tileDrawn(canvas);
+            document.getElementsByTagName('head')[0].removeChild(layer._scripts[key]);
+            delete layer._scripts[key];
+            
+        }
 
-		Kothic.render(canvas, data, zoom + zoomOffset, layer._additionalStyle, onRenderComplete, buffered);
-		delete this._canvases[key];
-	},
+        this._invertYAxe(data);
 
-	getDebugStr: function(debugInfo, x, y, zoom) {
-		return '<b>tile ' + x + ':' + y + ':' + zoom + '</b><br />' +
-			'<table><tr><td>' + debugInfo.layersStyled + '</td><td>layers styled</td></tr>' +
-			'<tr><td>' + debugInfo.mapRendered + '</td><td>map rendered</td></tr>' +
-			'<tr><td>' + debugInfo.iconsAndTextRendered + '</td><td>icons/text rendered</td></tr>' +
-			'<tr><td>' + debugInfo.total + '</td><td>total</td></tr></table>';
-	},
+        var styles = this.options.styles;
 
-	getDebugMessages: function() {
-		return this._debugMessages;
-	},
+        Kothic.render(canvas, data, zoom + zoomOffset, {
+            styles: styles,
+            locales: ['pl','en'],
+            onRenderComplete: onRenderComplete
+        });
 
-	drawTile: function(canvas, tilePoint, zoom) {
-		var zoomOffset = this.options.zoomOffset,
-			key = [(zoom - zoomOffset), tilePoint.x, tilePoint.y].join('/');
+        delete this._canvases[key];
+    },
 
-		this._canvases[key] = canvas;
-		this._loadScript('http://osmosnimki.ru/vtile/' + key + '.js');
-	},
+    getDebugMessages: function() {
+        return this._debugMessages;
+    },
 
-	setAdditionalStyle: function(fn) {
-		this._additionalStyle = fn;
+    drawTile: function(canvas, tilePoint, zoom) {
+        var zoomOffset = this.options.zoomOffset,
+            key = [(zoom - zoomOffset), tilePoint.x, tilePoint.y].join('/'),
+            url=this._url.replace('{x}',tilePoint.x).
+                    replace('{y}',tilePoint.y).
+                    replace('{z}',zoom-zoomOffset);
+        this._canvases[key] = canvas;
+        this._scripts[key] = this._loadScript(url);
+    },
 
-		// TODO implement layer.redraw() in Leaflet
-		//this._map.getPanes().tilePane.empty = false;
-		if (this._map && this._map._container) {
-			this._reset();
-			this._update();
-		}
-	},
+    enableStyle: function(name) {
+        if (MapCSS.availableStyles.indexOf(name) >= 0 && this.options.styles.indexOf(name) < 0) {
+            this.options.styles.push(name);
+            this.redraw();
+        }
+    },
 
-	_loadScript: function(url) {
-		var script = document.createElement('script');
-		script.src = url;
-		script.charset = 'utf-8';
-		document.getElementsByTagName('head')[0].appendChild(script);
-	}
+    disableStyle: function(name) {
+        if (this.options.styles.indexOf(name) >= 0) {
+            var i = this.options.styles.indexOf(name);
+            this.options.styles.splice(i, 1);
+            this.redraw();
+        }
+    },
+
+    redraw: function() {
+        MapCSS.invalidateCache();
+        // TODO implement layer.redraw() in Leaflet
+        this._map.getPanes().tilePane.empty = false;
+        if (this._map && this._map._container) {
+            this._reset();
+            this._update();
+        }
+    },
+
+    _invertYAxe: function(data) {
+        var type, coordinates, tileSize = data.granularity, i, j, k, l, feature;
+        for (i = 0; i < data.features.length; i++) {
+            feature = data.features[i];
+            coordinates = feature.coordinates;
+            type = data.features[i].type;
+            if (type === 'Point') {
+                coordinates[1] = tileSize - coordinates[1];
+            } else if (type === 'MultiPoint' || type === 'LineString') {
+                for (j = 0; j < coordinates.length; j++) {
+                    coordinates[j][1] = tileSize - coordinates[j][1];
+                }
+            } else if (type === 'MultiLineString' || type === 'Polygon') {
+                for (k = 0; k < coordinates.length; k++) {
+                    for (j = 0; j < coordinates[k].length; j++) {
+                        coordinates[k][j][1] = tileSize - coordinates[k][j][1];
+                    }
+                }
+            } else if (type === 'MultiPolygon') {
+                for (l = 0; l < coordinates.length; l++) {
+                    for (k = 0; k < coordinates[l].length; k++) {
+                        for (j = 0; j < coordinates[l][k].length; j++) {
+                            coordinates[l][k][j][1] = tileSize - coordinates[l][k][j][1];
+                        }
+                    }
+                }
+            } else {
+                throw "Unexpected GeoJSON type: " + type;
+            }
+
+            if (feature.hasOwnProperty('reprpoint')) {
+                feature.reprpoint[1] = tileSize - feature.reprpoint[1];
+            }
+        }
+    },
+
+    _loadScript: function(url) {
+        var script = document.createElement('script');
+        script.src = url;
+        script.charset = 'utf-8';
+        document.getElementsByTagName('head')[0].appendChild(script);
+        return script;
+    }
 });
